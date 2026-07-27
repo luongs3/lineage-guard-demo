18ed6d47d0660fb132a46f40a13f852e4b7f8300
luongs3 <luongr3@gmail.com>
2026-07-27 17:47:09 +0700

    Trigger re-review

--- migrations/2026_07_27_drop_order_total.sql
+++ migrations/2026_07_27_drop_order_total.sql
@@ -1,3 +1,4 @@
 -- Deprecating the redundant order_total column.
 -- It duplicates what downstream consumers can compute from line items.
 ALTER TABLE analytics.order_details DROP COLUMN order_total;
+-- rerun 1785149229
