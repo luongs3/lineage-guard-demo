eb8dd27eb18e4497c61273f7185f9b9a13081672
luongs3 <luongr3@gmail.com>
2026-07-27 17:41:02 +0700

    Drop redundant order_total column

---
+++ migrations/2026_07_27_drop_order_total.sql
@@ -0,0 +1,3 @@
+-- Deprecating the redundant order_total column.
+-- It duplicates what downstream consumers can compute from line items.
+ALTER TABLE analytics.order_details DROP COLUMN order_total;
